b8ab6b55208192760ec99645b3639bf34c3a9efa
Jade Turner <spacey-sooty@proton.me>
2024-08-31 22:29:59 +0800

    cleanup repo and improve build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
-docker import ./systemimage.tar.gz roborio:tmp
+unzip "FRC_roboRIO_$1.zip" -d unpack
+unzip "unpack/FRC_roboRIO_$1/roboRIO_$1" -d unpack
+
+docker import ./unpack/systemimage.tar.gz roborio:tmp
 docker build -f ./Dockerfile -t roborio:local .
 docker rmi roborio:tmp
+rm unpack -rf
 docker tag roborio:local roborio:latest
 docker compose up
